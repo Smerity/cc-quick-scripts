bd9a30419082895b53ef9f0916df5f232e082bb6
Stephen Merity <smerity@smerity.com>
2014-07-15 20:40:36 -0700

    `count_responses` update: retrieves and processes a single file

--- count_responses.sh
+++ count_responses.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# Returns total responses on stdout
-# Returns total bytes read on stderr
+# Saves total response count to responses folder
+# Returns total byte count to size folder
 # Naive testing showed wc --bytes faster than dd or pv (with pv not being 100% accurate)
 #
-zcat /dev/stdin | tee >(wc --bytes | awk '{ printf "Bytes:\t" $1 "\n" }' > /dev/stderr) | grep "WARC-Type: response" | wc -l | awk '{ printf "Responses:\t" $1 "\n" }' > /dev/stdout
+mkdir -p responses
+mkdir -p size
+s3cmd get s3://aws-publicdatasets/$1 - | zcat | tee >(wc --bytes > size/`basename $1`) | grep "WARC-Type: response" | wc -l > responses/`basename $1`
+# Helpful:
+# As there's no good error checking, errors sometimes pop up leaving empty or tiny results
+# cat * | grep -E '[0-9]{6,10}' | wc -l
+# cat * | grep -E '[0-9]{6,10}' | awk '{ SUM += $1} END { print SUM }'
+###
+# cat * | grep -E '[0-9]{4,10}' | wc -l
+# cat * | grep -E '[0-9]{4,10}' | awk '{ SUM += $1} END { print SUM }'
