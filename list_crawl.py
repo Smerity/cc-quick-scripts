db7043e81301744b2c280211ae8a6954bb3ed26a
Stephen Merity <smerity@smerity.com>
2014-07-26 17:29:20 -0700

    list_crawl.py: Records and plots individual file sizes for WARC/WAT/WET

--- list_crawl.py
+++ list_crawl.py
@@ -1,15 +1,18 @@
 import os
 import sys
 ###
+from prettyplotlib import plt
+###
 import boto
 conn = boto.connect_s3(anon=True)
 pds = conn.get_bucket('aws-publicdatasets')
 
 # Get all segments
-segments = list(pds.list('common-crawl/crawl-data/CC-MAIN-2014-23/segments/', delimiter='/'))
+target = 'CC-MAIN-2014-23'
+segments = list(pds.list('common-crawl/crawl-data/{}/segments/'.format(target), delimiter='/'))
 # Record the total size and all file paths for the segments
 files = dict(warc=[], wet=[], wat=[], segment=[x.name for x in segments])
-size = dict(warc=0, wet=0, wat=0)
+size = dict(warc=[], wet=[], wat=[])
 
 # Traverse each segment and all the files they contain
 for i, segment in enumerate(segments):
@@ -17,26 +20,37 @@ for i, segment in enumerate(segments):
   for ftype in ['warc', 'wat', 'wet']:
     for f in pds.list(segment.name + ftype + '/'):
       files[ftype].append(f.name)
-      size[ftype] += f.size
+      size[ftype].append(f.size)
 sys.stderr.write('\n')
 
 # Write total size and file paths to files
-if not os.path.exists('./crawl_stats/'):
-  os.makedirs('./crawl_stats')
+prefix = 'crawl_stats/{}/'.format(target)
+if not os.path.exists(prefix):
+  os.makedirs(prefix)
 ###
-f = open('crawl_stats/crawl.size', 'w')
+f = open(prefix + 'crawl.size', 'w')
 for ftype, val in size.items():
-  f.write('{}\t{}\n'.format(ftype, val))
+  f.write('{}\t{}\n'.format(ftype, sum(val)))
 f.close()
 ###
 for ftype in files:
-  f = open('crawl_stats/{}.path'.format(ftype), 'w')
+  f = open(prefix + '{}.path'.format(ftype), 'w')
   for fn in files[ftype]:
     f.write(fn + '\n')
   f.close()
 ###
 # Kid friendly stats (i.e. console)
 for ftype, fsize in size.items():
-  print '{} files contain {} bytes over {} files'.format(ftype, fsize, len(files[ftype]))
+  print '{} files contain {} bytes over {} files'.format(ftype.upper(), sum(fsize), len(files[ftype]))
 ###
 # Note: you might want to run `gzip *` before upload
+
+###
+# Plot
+for ftype, fsize in size.items():
+  plt.hist(fsize, bins=50)
+  plt.xlabel('Size (bytes)')
+  plt.ylabel('Count')
+  plt.title('Distribution for {}'.format(ftype.upper()))
+  plt.savefig('crawl_stats/{}_dist.pdf'.format(ftype))
+  plt.show(block=True)
