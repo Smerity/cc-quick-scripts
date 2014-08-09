a08133b4e73c1e5a883f658378033c88103f0364
Stephen Merity <smerity@smerity.com>
2014-08-09 16:20:30 -0700

    list_crawl now finds missing WAT+WET files and lists them for creation

--- list_crawl/list_crawl.py
+++ list_crawl/list_crawl.py
@@ -1,6 +1,8 @@
 import os
 import sys
 ###
+from collections import defaultdict
+###
 from prettyplotlib import plt
 ###
 import boto
@@ -8,7 +10,7 @@ conn = boto.connect_s3(anon=True)
 pds = conn.get_bucket('aws-publicdatasets')
 
 # Get all segments
-target = 'CC-MAIN-2014-23'
+target = 'CC-MAIN-2014-15'
 segments = list(pds.list('common-crawl/crawl-data/{}/segments/'.format(target), delimiter='/'))
 # Record the total size and all file paths for the segments
 files = dict(warc=[], wet=[], wat=[], segment=[x.name for x in segments])
@@ -41,9 +43,10 @@ for ftype in files:
 ###
 # Kid friendly stats (i.e. console)
 for ftype, fsize in size.items():
-  print '{} files contain {} bytes over {} files'.format(ftype.upper(), sum(fsize), len(files[ftype]))
+  sys.stderr.write('{} files contain {} bytes over {} files\n'.format(ftype.upper(), sum(fsize), len(files[ftype])))
 ###
-# Note: you might want to run `gzip *` before upload
+# Note: you might want to run `gzip *.zip` before upload
+# s3cmd put --acl-public *.path.gz s3://aws-publicdatasets/common-crawl/crawl-data/CC-MAIN-YYYY-WW/
 
 ###
 # Plot
@@ -53,4 +56,28 @@ for ftype, fsize in size.items():
   plt.ylabel('Count')
   plt.title('Distribution for {}'.format(ftype.upper()))
   plt.savefig(prefix + '{}_dist.pdf'.format(ftype))
-  plt.show(block=True)
+  #plt.show(block=True)
+  plt.close()
+
+###
+# Find missing WAT / WET files
+warc = set([x.strip() for x in open(prefix + 'warc.path').readlines()])
+wat = [x.strip() for x in open(prefix + 'wat.path').readlines()]
+wat = set([x.replace('.warc.wat.', '.warc.').replace('/wat/', '/warc/') for x in wat])
+# Work out the missing files and segments
+missing = sorted(warc - wat)
+missing_segments = defaultdict(list)
+for fn in missing:
+  start, suffix = fn.split('/warc/')
+  segment = start.split('/')[-1]
+  missing_segments[segment].append(fn)
+# Save the files such that we can run a new WEATGenerator job
+prefix += 'weat.queued/'
+if not os.path.exists(prefix):
+  os.mkdir(prefix)
+sys.stderr.write('Total of {} missing segments with {} missing files\n'.format(len(missing_segments), len(warc - wat)))
+for seg, files in missing_segments.iteritems():
+  #sys.stderr.write('{} has {} missing parts\n'.format(seg, len(files)))
+  f = open(prefix + 'seg_{}'.format(seg), 'w')
+  [f.write('s3a://aws-publicdatasets/{}\n'.format(fn)) for fn in files]
+  f.close()
